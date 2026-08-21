f3c94f13ef56cd77af0ed62b943b6a81e613da49
akashkukreti38-lgtm <akashkukreti38@gmail.com>
2026-08-21 23:14:15 +0530

    Add PDF extraction and OCR pipeline

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI, UploadFile, File
 from pydantic import BaseModel
 import os
+import fitz
+import pytesseract
+from PIL import Image
+
+pytesseract.pytesseract.tesseract_cmd = (
+    r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+)
+
 
 app = FastAPI()
 
@@ -29,10 +37,54 @@ def chat(request: ChatRequest):
 
 @app.post("/documents/upload")
 async def upload_document(file: UploadFile = File(...)):
-    file_path=os.path.join("backend","uploads", file.filename)
-    with open(file_path,"wb") as buffer:
+
+    if file.content_type != "application/pdf":
+        return {
+            "error": "Only PDF files are allowed"
+        }
+
+    file_path = os.path.join("backend", "uploads", file.filename)
+
+    with open(file_path, "wb") as buffer:
         buffer.write(await file.read())
+
+    text=extract_text_from_pdf(file_path)
+
     return {
+        "message": "File uploaded successfully",
         "filename": file.filename,
-        "content_type": file.content_type
+        "text_preview": text[:500]
     }
+
+"""def extract_text_from_pdf(file_path):
+    document=fitz.open(file_path)
+    text=""
+    for page in document:
+        text+=page.get_text()
+    document.close()
+    return text"""
+
+def extract_text_from_pdf(file_path):
+    document = fitz.open(file_path)
+
+    text = ""
+
+    for page in document:
+        page_text = page.get_text()
+
+        if page_text.strip():
+            text += page_text
+        else:
+            pix = page.get_pixmap()
+            image = Image.frombytes(
+                "RGB",
+                [pix.width, pix.height],
+                pix.samples
+            )
+
+            ocr_text = pytesseract.image_to_string(image)
+            text += ocr_text
+
+    document.close()
+
+    return text
